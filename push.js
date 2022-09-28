019062364dbbba10f525a3677b2e422215555ed8
Ulf Zackrisson <ulfzackrisson@Ulfs-MacBook-Air-2.local>
2022-09-28 15:36:10 +0200

    changed the reg key again

--- push.js
+++ push.js
@@ -1,8 +1,9 @@
 let push = require('web-push');
 
+// ********************
 let vapidKeys = {
-    publicKey: 'BHFVGLmr49ucXSO-nRj1xwSYkUz1H6GyNBzlY4HEvfZOi6_u0uZ76B06JRcuARGa-8KPZmuCFps5dpzjjDI5jdY',
-    privateKey: '5uBNZd7E5qAw45_X_Q2nhRjikcVUxEPn2NzZgD2Eass'
+    publicKey: 'BAWwzaQSWnr902LMsGhy9RIsCDOA2X1t2AJ1qjU0vtDAQCsu_oKGuLtE-vOxl4y1sfP1BNwTIQVnoXBCFPdTAHo',
+    privateKey: 'A65QuFO6sXwCp13C1lgp70GqYZ9dueAcV6nDQKPM6HA'
 };
 
 push.setVapidDetails('mailto:ulmezz@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
@@ -10,7 +11,10 @@ push.setVapidDetails('mailto:ulmezz@gmail.com', vapidKeys.publicKey, vapidKeys.p
 let sub = {};
 
 push.sendNotification(sub, 'test message');
+// ********************
 
+// ********************
 // let vapidKeys = push.generateVAPIDKeys();
 
 // console.log(vapidKeys);
+// ********************
