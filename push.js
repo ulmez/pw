9e7721c2825daeabb6d2033b8ab99f6a1719cddf
Ulf Zackrisson <ulfzackrisson@Ulfs-MacBook-Air-2.local>
2022-09-28 16:08:25 +0200

    major changes

--- push.js
+++ push.js
@@ -1,4 +1,4 @@
-let push = require('web-push');
+let webpush = require('web-push');
 
 // ********************
 let vapidKeys = {
@@ -6,18 +6,22 @@ let vapidKeys = {
     privateKey: 'A65QuFO6sXwCp13C1lgp70GqYZ9dueAcV6nDQKPM6HA'
 };
 
-push.setVapidDetails('mailto:ulmezz@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
+webpush.setVapidDetails(
+    'mailto:ulmezz@gmail.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
 
-let sub = {
+let pushSubscription = {
     endpoint: 'https://fcm.googleapis.com/fcm/send/cphKZWDobx0:APA91bELlqVyyD1GFXUXBzBrQObTKnkarMRjH5O3WjhxGg7C6x0pz5slzyRexExiL5ECU8QezfVDMtpldzfIY7LDfrtuqZb8KYgRTAJ4J1fsmOw8UvjpWOq2AzRL15hF32iLJyzvJEQ0',
-    expirationTime: null,
+    // expirationTime: null,
     keys: {
         p256dh:'BI-A2prVynJ_Z4CtbEyVia_iXy4ZvoGGJsBaXTfIZ6cb6qfypUJ-6iGPhhHFVgbWIomn-FXv1Tkcph9JaIP5xgE',
         auth:'1hsQC-i2_qs7yL1qsEifKQ'
     }
 };
 
-push.sendNotification(sub, 'test message');
+webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
 // ********************
 
 // ********************
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 self.addEventListener('push', () => {
-    self.registration.sendNotification('test message', {});
+    self.registration.showNotification('Hello world!', {});
 });
