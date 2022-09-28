3205e6bc08b291f92d75a1ee2ba2ff9c3869f0ba
Ulf Zackrisson <ulfzackrisson@Ulfs-MacBook-Air-2.local>
2022-09-28 23:59:16 +0200

    add 3 showNotifications

--- sw.js
+++ sw.js
@@ -1,5 +1,19 @@
 self.addEventListener('push', () => {
-    self.registration.showNotification('Hello world!', {
+    self.registration.showNotification('Hello world 1!', {
+        body: 'Notified by Traversy Media',
+        icon: 'http://image.ibb.co/frYOFd/tmlogo.png'
+    });
+});
+
+self.addEventListener('push', () => {
+    self.registration.showNotification('Hello world 2!', {
+        body: 'Notified by Traversy Media',
+        icon: 'http://image.ibb.co/frYOFd/tmlogo.png'
+    });
+});
+
+self.addEventListener('push', () => {
+    self.registration.showNotification('Hello world 3!', {
         body: 'Notified by Traversy Media',
         icon: 'http://image.ibb.co/frYOFd/tmlogo.png'
     });
