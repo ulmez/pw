87f059d19b3b464b7b4d50121b9bf6dd4c1993f5
Ulf Zackrisson <ulfzackrisson@Ulfs-MacBook-Air-2.local>
2022-09-28 14:47:16 +0200

    changed applicationServerKey

--- push.js
+++ push.js
@@ -1,8 +1,8 @@
 let push = require('web-push');
 
 let vapidKeys = {
-    publicKey: 'BGcNiUs1HK9Xi3KC_otPHL-mjzBdnT8aF7K1Xn-1DOOQF4EQG4MUVIENIKaWLU8KFj6iJxm_fH3o43MdvzRwakA',
-    privateKey: 'nxit6PBvLyJrHtMIXXzmgSLVfgSqw_Z07pi0EbRy7Uw'
+    publicKey: 'BHFVGLmr49ucXSO-nRj1xwSYkUz1H6GyNBzlY4HEvfZOi6_u0uZ76B06JRcuARGa-8KPZmuCFps5dpzjjDI5jdY',
+    privateKey: '5uBNZd7E5qAw45_X_Q2nhRjikcVUxEPn2NzZgD2Eass'
 };
 
 push.setVapidDetails('mailto:ulmezz@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
